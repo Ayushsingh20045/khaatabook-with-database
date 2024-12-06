1eed2ecf3404c07cd0592d50516fb06bacc081b1
Ayush Singh <ayush440896@gmail.com>
2024-12-06 11:19:28 +0530

    login and signup complete

--- app.js
+++ app.js
@@ -1,47 +1,46 @@
-const express = require('express')
+const express = require("express");
 const app = express();
-const bodyParser=require('body-parser')
-const path=require('path')
-const connectDB=require('./config/mongoose')
-const user=require('./models/user')
-
+const bodyParser = require("body-parser");
+const path = require("path");
+const connectDB = require("./config/mongoose");
+const user = require("./models/user");
 
 //middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
-app.set("view engine","ejs");
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
 
 //routes
 
 //render signup page
-app.get('/signup',(req,res)=>{
-res.render('signup')
-})
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
 
 //handle signup form submission (post method)
 
-app.post('/signup',async(req,res)=>{
+app.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
 
-   const {username,password}=req.body;
+  //check if the user is already exists
 
-   //check if the user is already exists
+  const existingUser = await user.findOne({ username });
 
-   const existingUser= await user.findOne({username});
+  if (existingUser) {
+    return res.send("user already exixts.please try again");
+  }
+else{
 
-   if(existingUser){
-    return res.send('user already exixts.please try again')
-   }
+    //create a new user and save to the database
+    const newUser = user.create({ username, password });
+}
 
-   //create a new user and save to the database
-   const newUser=user.create({username,password});
-   
-   res.redirect('/login')
-})
+  res.redirect("/login");
+});
 
-app.get('/login',(req,res)=>{
-    res.render('login')
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
 app.listen(3000);
